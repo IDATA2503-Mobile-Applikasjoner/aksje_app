14cdfc980cd39626037b258b0f2f12400ec2d208
Viddyyy <sanderknutsen125@gmail.com>
2023-11-08 15:17:16 +0100

    list promts fixed(loading, log in, create list)

--- lib/widgets/screens/my_lists.dart
+++ lib/widgets/screens/my_lists.dart
@@ -1,4 +1,5 @@
 import 'package:aksje_app/models/stock_list_model.dart';
+import 'package:aksje_app/widgets/stock_list_components/stock_list_list.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
@@ -16,6 +17,7 @@ class MyListsPage extends StatefulWidget {
 
 class _MyListsPageState extends State<MyListsPage> {
   List<StockListModel> lists = [];
+  bool isLoading = true;
 
   @override
   void initState() {
@@ -28,58 +30,90 @@ class _MyListsPageState extends State<MyListsPage> {
     _fetchDataFromServer();
   }
 
-  void _fetchDataFromServer() async {
+  Future<void> _fetchDataFromServer() async {
     try {
-      UserProvider userProvider = Provider.of<UserProvider>(context);
+      UserProvider userProvider = Provider.of<UserProvider>(context, listen: false);
       var uid = userProvider.user!.uid;
-      print(uid);
       var baseURL = Uri.parse("http://10.0.2.2:8080/api/list/listsbyuid/$uid");
       var response = await http.get(baseURL);
 
       if (response.statusCode == 200) {
         List responseData = jsonDecode(response.body);
         setState(() {
-          lists = responseData
-              .map((data) => StockListModel.fromJson(data))
-              .toList();
+          lists = responseData.map((data) => StockListModel.fromJson(data)).toList();
+          isLoading = false;
         });
       } else {
         print('Request failed with status: ${response.statusCode}.');
+        isLoading = false;
       }
     } catch (e) {
       print(e);
+      isLoading = false;
     }
   }
 
-  @override
-  Widget build(BuildContext context) {
-    UserProvider userProvider = Provider.of<UserProvider>(context);
-    User? user = userProvider.user;
+@override
+Widget build(BuildContext context) {
+  UserProvider userProvider = Provider.of<UserProvider>(context);
+  User? user = userProvider.user;
+  if (userProvider.user == null) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('My Lists'),
       ),
-      body: ListView.builder(
-        itemCount: lists.length,
-        itemBuilder: (context, index) {
-          return ListTile(
-            leading: const Icon(Icons.list),
-            title: Text(lists[index].name),
-            onTap: () {
-              // Handle list tap
-            },
-          );
-        },
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          Navigator.push(
-            context,
-            MaterialPageRoute(builder: (context) => const AddListPage()),
-          );
-        },
-        child: const Icon(Icons.add),
+      body: const Center(
+        child: Padding(
+          padding: EdgeInsets.all(16.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Text("Login to see lists"),
+            ],
+          ),
+        ),
       ),
     );
   }
+
+  return Scaffold(
+    appBar: AppBar(
+      title: const Text('My Lists'),
+    ),
+    body: lists.isEmpty
+        ? Center(
+            child: isLoading
+                ? CircularProgressIndicator()
+                : const Padding(
+                    padding: EdgeInsets.all(16.0),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Text("No lists found. Press + button to add a new list"),
+                      ],
+                    ),
+                  ),
+          )
+        : Column(
+            children: <Widget>[
+              Expanded(
+                child: StockListModelList(
+                  stockLists: lists,
+                ),
+              )
+            ],
+          ),
+    floatingActionButton: FloatingActionButton(
+      onPressed: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) => const AddListPage()),
+        );
+      },
+      child: const Icon(Icons.add),
+    ),
+  );
+}
 }
